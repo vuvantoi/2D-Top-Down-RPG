888a51690a15d5b6af57c118a348d0cf9f6690a0
vuvantoi <your-email@example.com>
2026-06-10 10:50:24 +0700

    Player Health + Knockback

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -17,6 +17,7 @@ public class PlayerController : Singleton<PlayerController>
     private Rigidbody2D rb;
     private Animator myAnimator;
     private SpriteRenderer mySpriteRenderer;
+    private Knockback knockback;
     private float startingMoveSpeed;
 
     private bool facingLeft = false;
@@ -28,6 +29,7 @@ public class PlayerController : Singleton<PlayerController>
         rb = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
         mySpriteRenderer = GetComponent<SpriteRenderer>();
+        knockback = GetComponent<Knockback>();
     }
     private void OnEnable()
     {
@@ -65,6 +67,7 @@ public class PlayerController : Singleton<PlayerController>
 
     private void Move()
     {
+        if(knockback.GettingKnockedBack) {return;}
         rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
     }
 
